b8a84bc84178723eb476b718ab74bfb2abe6fb4d
uzair abid <uzairabid1001@gmial.com>
2022-02-05 16:39:23 +0500

    first commit

---
+++ public/app.js
@@ -0,0 +1,135 @@
+var img11 = document.getElementById("img1.1")
+var img12 = document.getElementById("img1.2")
+var img21 = document.getElementById("img2.1")
+var img22 = document.getElementById("img2.2")
+var img31 = document.getElementById("img3.1")
+var img32 = document.getElementById("img3.2")
+var img41 = document.getElementById("img4.1")
+var img42 = document.getElementById("img4.2")
+var img51 = document.getElementById("img5.1")
+var img52 = document.getElementById("img5.2")
+var img61 = document.getElementById("img6.1")
+var img62 = document.getElementById("img6.2")
+var img71 = document.getElementById("img7.1")
+var img72 = document.getElementById("img7.2")
+var img81 = document.getElementById("img8.1")
+var img82 = document.getElementById("img8.2")
+var img91 = document.getElementById("img9.1")
+var img92= document.getElementById("img9.2")
+var img101 = document.getElementById("img10.1")
+var img102 = document.getElementById("img10.2")
+var img111= document.getElementById("img11.1")
+var img112= document.getElementById("img11.2")
+var img121 = document.getElementById("img12.1")
+var img122 = document.getElementById("img12.2")
+
+function change() {
+    img11.style.display="none"
+    img12.style.display="block"
+}
+function chan() {
+    img11.style.display="block"
+    img12.style.display="none"
+}
+function change2() {
+    img21.style.display="none"
+    img22.style.display="block"
+}
+function chan2() {
+    img21.style.display="block"
+    img22.style.display="none"
+}
+function change3() {
+    img31.style.display="none"
+    img32.style.display="block"
+}
+function chan3() {
+    img31.style.display="block"
+    img32.style.display="none"
+}
+function change4() {
+    img41.style.display="none"
+    img42.style.display="block"
+}
+function chan4() {
+    img41.style.display="block"
+    img42.style.display="none"
+}
+function change5() {
+    img51.style.display="none"
+    img52.style.display="block"
+}
+function chan5() {
+    img51.style.display="block"
+    img52.style.display="none"
+}
+function change6() {
+    img61.style.display="none"
+    img62.style.display="block"
+}
+function chan6() {
+    img61.style.display="block"
+    img62.style.display="none"
+}
+function change7() {
+    img71.style.display="none"
+    img72.style.display="block"
+}
+function chan7() {
+    img71.style.display="block"
+    img72.style.display="none"
+}
+function change8() {
+    img81.style.display="none"
+    img82.style.display="block"
+}
+function chan8() {
+    img81.style.display="block"
+    img82.style.display="none"
+}
+function change9() {
+    img91.style.display="none"
+    img92.style.display="block"
+}
+function chan9() {
+    img91.style.display="block"
+    img92.style.display="none"
+}
+function change10() {
+    img101.style.display="none"
+    img102.style.display="block"
+}
+function chan10() {
+    img101.style.display="block"
+    img102.style.display="none"
+}
+function change11() {
+    img111.style.display="none"
+    img112.style.display="block"
+}
+function chan11() {
+    img111.style.display="block"
+    img112.style.display="none"
+}
+function change12() {
+    img121.style.display="none"
+    img122.style.display="block"
+}
+function chan12() {
+    img121.style.display="block"
+    img122.style.display="none"
+}
+var ddd = document.getElementById("ddd")
+var diskk = document.getElementById("diskk")
+// var ordert = document.getElementById("ordert")
+
+
+function disk() {
+    ddd.style.display="none"
+    diskk.style.display="block"
+}
+                                                                                                                              
+function order(id){
+    orderd.style.display="block"
+    order(id)   
+}              
